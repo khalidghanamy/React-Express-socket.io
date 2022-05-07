290bd26ef74142285f3a1271d9b24637a36070e4
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-05-07 16:49:18 +0200

    create rooms , send and receive data

--- server/index.js
+++ server/index.js
@@ -20,7 +20,14 @@ const io =new Server (server,{
 
 io.on("connection",(socket)=>{
     console.log(`user connected :${socket.id}`);
-    socket.on('send_message',(data)=>{
-        socket.broadcast.emit("receive_message",data)
+
+    socket.on('join_room',(data)=>{
+        socket.join(data)
     })
+
+
+    socket.on('send_message',(data)=>{
+        socket.to(data.room).emit("receive_message",data)
+    })
+   
 })
