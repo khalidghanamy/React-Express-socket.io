523995f3bbabd05c89a44d5f4c270003411b3241
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-05-07 15:25:00 +0200

    server is up

---
+++ server/index.js
@@ -0,0 +1,19 @@
+ const express=require('express')
+ const app = express()
+const http=require('http')
+const cors =require('cors')
+const {Server}=require('socket.io')
+
+app.use(cors());
+
+const server=http.createServer(app)
+      server.listen(4000,()=>{
+          console.log('server is up');
+      })
+
+const io =new Server (server,{
+    cors:{
+        origin:'http://localhost:3000',
+        methods: ["GET","POSt"]
+    }
+})
